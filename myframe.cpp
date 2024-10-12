b686f9615434a1da3032f81b11191b9c7d155c42
Rodrigo Araujo <araujo.rm@gmail.com>
2024-10-12 01:14:06 +0100

    Add GPLv2 license
    Also remove my old loooong gone email from the about section.
    Those were simpler times...

--- myframe.cpp
+++ myframe.cpp
@@ -1,3 +1,22 @@
+/*
+sueca - An implementation of the Portuguese game "Sueca" in C++ and wxWidgets
+Copyright (C) 2003-2024 Rodrigo Araujo
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include "myframe.hpp"
 #include <wx/msgdlg.h>
 #include "prefsdialog.hpp"
@@ -181,8 +200,18 @@ void MyFrame::Help( wxCommandEvent& event )
 void MyFrame::About( wxCommandEvent& event )
 {
   wxMessageDialog md( this, VERSION_STRING "\n\
-(C) 2003, 2004 Rodrigo Araujo <rmaraujo@dcc.online.pt>\n\
-Card graphics by jfitz technologies - http://www.jfitz.com/",
+(C) 2003-2024 Rodrigo Araujo\n\
+Card graphics by jfitz technologies - http://www.jfitz.com/\n\
+\n\
+This program is free software; you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation; either version 2 of the License, or\n\
+(at your option) any later version.\n\
+\n\
+This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+GNU General Public License for more details.",
                       "About this game", wxOK );
   md.ShowModal();
 }
--- prefsdialog.cpp
+++ prefsdialog.cpp
@@ -1,3 +1,22 @@
+/*
+sueca - An implementation of the Portuguese game "Sueca" in C++ and wxWidgets
+Copyright (C) 2003-2024 Rodrigo Araujo
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include "prefsdialog.hpp"
 #include <wx/sizer.h>
 #include <wx/stattext.h>
@@ -16,7 +35,7 @@ PrefsDialog::PrefsDialog( wxWindow* parent ):
   wxDialog( parent, wxID_ANY, wxString( "Game Preferences" ) )
 {
   // Name entry
-  wxBoxSizer* name_sizer = new wxBoxSizer( wxHORIZONTAL );  
+  wxBoxSizer* name_sizer = new wxBoxSizer( wxHORIZONTAL );
   name_sizer->Add( new wxStaticText( this, wxID_ANY, "Player name" ), 0, wxRIGHT | wxALIGN_CENTER, 5 );
   name_entry = new wxTextCtrl( this, wxID_ANY, wxGetApp().GetLocalPlayerName() );
   name_entry->SetMaxLength( PLAYER_NAME_MAX );
